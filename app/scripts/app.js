8ca7aed448bc0b1709ce66d9b0fab69728746230
David Gudeman <davidmgudeman@gmail.com>
2016-06-12 10:14:26 -0700

    This is a working configuration using Gulp task runner.  This uses browser sync instead of connect functionality to livereload. It also has been updated to use the  and got rid of the need to alias the controller and refer to variables as menuCtrl.* because now the view and controller are connected with

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,10 +4,15 @@
 'use strict';
 angular.module('confusionApp', [])
 
-    .controller('menuController', function() {
-        this.tab = 1;
-        this.filtText = '';
-        var dishes=[
+    .controller('MenuController', ['$scope',function($scope) {
+        $scope.tab = 1;
+        $scope.filtText = '';
+        $scope. showDetails = false;
+        $scope.toggleDetails = function() {
+            $scope.showDetails = !$scope.showDetails;
+        };
+
+         var dishes=[
             {
                 name:'Uthapizza',
                 image: 'images/uthapizza.png',
@@ -45,25 +50,26 @@ angular.module('confusionApp', [])
                 comment: ''
             }
         ];
-        this.dishes = dishes;
 
-        this.select = function(setTab) {
-            this.tab = setTab;
+        $scope.dishes = dishes;
+
+        $scope.select = function(setTab) {
+            $scope.tab = setTab;
             if (setTab === 2) {
-                this.filtText = "appetizer";
+                $scope.filtText = "appetizer";
             }
             else if (setTab === 3) {
-                this.filtText = "mains";
+                $scope.filtText = "mains";
             }
             else if (setTab === 4) {
-                this.filtText = "dessert";
+                $scope.filtText = "dessert";
             }
             else {
-                this.filtText = "";
+                $scope.filtText = "";
             }
         };
-        this.isSelected = function (checkTab) {
-            return (this.tab === checkTab);
+        $scope.isSelected = function (checkTab) {
+            return ($scope.tab === checkTab);
         };
-    });
+    }]);
 
